e2a77de3eb407ad51309dba0438e4b1930570863
Javed Sameer <js@Javids-iMac.local>
2020-12-18 22:09:54 +0530

    cleaning code

--- signup_page/script.js
+++ signup_page/script.js
@@ -1,30 +1,53 @@
-const formElement = document.getElementById('form')
+/* validation of form*/
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const firstname = formElement['firstname'].value
-  const lastname = formElement['lastname'].value
-  const email = formElement['email'].value
-  const password = formElement['password'].value
-  console.log(firstname)
-  if (firstname === '') {
-    formElement['firstname'].parentElement.classList.add('error')
-  } else {
-    formElement['firstname'].parentElement.classList.remove('error')
-  }
-  if (lastname === '') {
-    formElement['lastname'].parentElement.classList.add('error')
-  } else {
-    formElement['lastname'].parentElement.classList.remove('error')
-  }
-  if (email === '') {
-    formElement['email'].parentElement.classList.add('error')
-  } else {
-    formElement['email'].parentElement.classList.remove('error')
-  }
-  if (password === '') {
-    formElement['password'].parentElement.classList.add('error')
-  } else {
-    formElement['password'].parentElement.classList.remove('error')
-  }
+window.addEventListener('load', function() {
+  const form = document.getElementById('form')
+  const username = document.getElementById('username')
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
+  const password2 = document.getElementById('password2')
+
+  form.addEventListener('click', (e) => {
+    e.preventDefault()
+    checkInputs()
+  })
 })
+
+function checkInputs() {
+  const usernameValue = username.value.trim()
+  const emailValue = email.value.trim()
+  const passwordValue = password.value.trim()
+  const password2Value = password2.value.trim()
+  if (usernameValue === '') {
+    setError(username, 'user cannot be blank')
+  } else {
+    setSuccess(username)
+  }
+  if (passwordValue === '') {
+    setError(password, 'password cannot be blank')
+  } else {
+    setSuccess(password)
+  }
+  if (emailValue === '') {
+    setError(email, 'email cannot be blank')
+  } else {
+    setSuccess(email)
+  }
+  if (password2Value === '' || password2Value !== passwordValue) {
+    setError(password2, 'password2 cannot be blank')
+  } else {
+    setSuccess(password2)
+  }
+}
+
+function setError(input, message) {
+  const formControl = input.parentElement
+  const small = document.querySelector('small')
+  small.innerText = message
+  formControl.className = 'form-control error'
+}
+
+function setSuccess(input) {
+  const formControl = input.parentElement
+  formControl.className = 'form-control success'
+}
